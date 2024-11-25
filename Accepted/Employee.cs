77d9436bc030ab5e3f4cf06ac17ce33b60b4811c
Ashraf Abu Elrai <126774761+ashrafabuelrai@users.noreply.github.com>
2024-11-25 19:32:03 +0200

    Master

---
+++ Accepted/Employee.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accepted
+{
+    class Employee
+    {
+        public int ID { get; internal  set; }
+        public string Name { get; internal  set; }
+        public decimal Salary { get; internal  set; }
+
+        public override bool Equals(object? obj)
+        {
+            Employee? temp = obj as Employee;
+            if (temp == null) return false;
+            if (this.GetType() != temp.GetType()) return false;
+            if (ReferenceEquals(this, temp)) return true;
+            return this.ID == temp.ID && this.Name == temp.Name && this.Salary == temp.Salary;
+        }
+        public override string ToString() => $"ID:{ID} , Name:{Name} , Salary{Salary}";
+    }
+}
+*/
---
+++ Accepted/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using Common;
+using System.Net.Http.Headers;
+namespace Accepted
+{
+    
+    internal class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int y = 30;
+        }
+    }
+}
